398f5f840eac812cf3d1a5ee56081599248e04b5
SalarUW <salarahmaduw@gmail.com>
2019-05-19 16:01:50 -0400

    Refactored a little, added some comments

--- src/com/salar/algo240book/Main.java
+++ src/com/salar/algo240book/Main.java
@@ -13,18 +13,20 @@ public class Main {
         // int N = 200;
         // UnionFind uf = new UnionFind(N);
 
-        InsertionSort insertionSort = new InsertionSort();
+        // Well this took longer to figure out than it should have
+        // I need an array of Integers and this is the best way I came up with
+        // Create a List object and then populate a list with the Integers one by one -_-
+        // Has to be a better way of doing this
 
         List<Integer> simpleList = Arrays.asList(10, 11, 89, 12, 49, 48, 56, 20, 9);
-
         Integer[] simpleArray = new Integer[simpleList.size()];
+        InsertionSort insertionSort = new InsertionSort();
 
         for (int i = 0; i < simpleList.size(); i++) {
             simpleArray[i] = simpleList.get(i);
         }
 
         insertionSort.sort(simpleArray);
-
         for (Integer j : simpleArray) System.out.println(j);
     }
 }
